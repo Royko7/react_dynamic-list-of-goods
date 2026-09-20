2dc294e439c352f58966efbbc2f6d29a7d6eb4ba
Royko7 <ura.royko@gmail.com>
2026-09-20 18:28:45 +0300

    Add solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [goodList, setGoodlist] = useState<Good[]>([]);
+  const [mode, setMode] = useState('');
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  useEffect(() => {
+    switch (mode) {
+      case 'All':
+        getAll().then(goods => setGoodlist(goods));
+        break;
+      case '5First':
+        get5First().then(goods => setGoodlist(goods));
+        break;
+      case 'RedGoods':
+        getRedGoods().then(goods => setGoodlist(goods));
+        break;
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+      default:
+        return;
+    }
+  }, [mode]);
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
 
-    <GoodsList goods={[]} />
-  </div>
-);
+      <button type="button" data-cy="all-button" onClick={() => setMode('All')}>
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={() => setMode('5First')}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={() => setMode('RedGoods')}
+      >
+        Load red goods
+      </button>
+
+      <GoodsList goods={goodList} />
+    </div>
+  );
+};
